6ce764c59c16349fff843504a908d20849fb47e4
Brendan Alger <bren@wifi-131-179-14-161.host.ucla.edu>
2019-06-27 16:59:50 -0700

    Changed color scheme slightly

--- collapse.js
+++ collapse.js
@@ -33,7 +33,7 @@ for (var i = 0; i < collapsibles.length; i++) {
 /* remove_home gets the homepage div box and gets the coordinates 
  * If the coordinates end up being outside the screen then the div gets
  * removed so that it doesn't get scrolled up to again. */
-var remove_home = 
+/*var remove_home = 
     function() {
         // Save the first div with id 'entrance'
         var elem = document.querySelector('#entrance');
@@ -52,16 +52,18 @@ var remove_home =
             window.removeEventListener("scroll", remove_home);
         }
     };
-
+*/
 // Check the window every time it is scrolled to see if the homepage
 // is off the screen.
-window.addEventListener("scroll", remove_home);
+//window.addEventListener("scroll", remove_home);
 
 // Animation for entrance to site.
-/* Mainpage banner is clicked then scrolls down to actual page.
+/* 
+ Mainpage banner is clicked then scrolls down to actual page.
  * Once scrolled down to actual page, banner is closed.
  */
 // Scrolls down to link instead of immediately going to it.
+/*
 jQuery('.scroll').click(function(e){
     var jump = $(this).attr('href');
     var new_position = $(jump).offset();
@@ -72,3 +74,4 @@ jQuery('.scroll').click(function(e){
     
 
 });
+*/
